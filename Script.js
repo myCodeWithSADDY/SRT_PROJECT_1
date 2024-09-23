3064515aa2ef8fdb1ffedebfdf1144998c2ac3ab
myCodeWithSADDY <wuzzydetective@gmail.com>
2024-09-23 18:20:24 +0500

    first commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+let display = document.querySelector(".display")
+
+function submit(){
+    display.classList.add("open-display")
+}
+function confirm(){
+    display.classList.remove("open-display")
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { 
+      backgroundImage:{
+        'custom-img': "url('/background.jpg')" 
+
+      }
+    },
+  },
+  plugins: [],
+}
